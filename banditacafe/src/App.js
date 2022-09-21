ad2643bbfed33cca8a122e4909fb2b473be91083
Paola Vazquez <paovats@gmail.com>
2022-09-21 08:05:03 -0500

    eslogan y valores

--- banditacafe/src/App.js
+++ banditacafe/src/App.js
@@ -6,10 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bandita Café
-        </p>
       </header>
+      <p className="eslogan">"El lugar ideal para tus co-meetings"</p>
       <div>
         <h1>MISIÓN</h1>
         <p>Proporcionar un lugar cómodo y tranquilo para dejar fluir las ideas en equipo y lograr los objetivos colectivos</p>
@@ -17,6 +15,18 @@ function App() {
       <div>
         <h1>VISIÓN</h1>
         <p>Lograr que los meetings sean todo menos tediosos y aburridos</p>
+      </div>
+      <div>
+        <h1>VALORES</h1>
+          <div className="valores">
+            <p>Transparencia</p>
+            <p>Pasión</p>
+            <p>Claridad</p>
+            <p>Responsabipdad</p>
+            <p>Escucha</p>
+            <p>Puntuapdad</p>
+            <p>Lealtad</p>
+          </div>
       </div>
     </div>
   );
